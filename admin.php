a386963c2caffa1857fea51d77ec1a5ded6c8a2b
fpeyret <fpeyret@axome.com>
2018-07-11 15:19:56 +0200

    start admin

---
+++ admin.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Administration - Miscreated Interactive Map</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/admin.css">
+
+	</head>
+	<body>
+		<?php
+			include('admin/pages/connexion.php');
+		?>
+		<script type="text/javascript" src="assets/js/jquery.js"></script>
+		<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+	</body>
+</html>
---
+++ admin/pages/connexion.php
@@ -0,0 +1,21 @@
+<div class="container">
+	<div class="row">
+		<div class="col-xs-12 col-sm-offset-4 col-sm-4">
+			<br><br>
+			<h1 class="page-header text-center">Administration</h1>
+			<div class="panel panel-primary">
+				<div class="panel-body">
+					<div class="form-group">
+						<label for="login_name">Email address</label>
+						<input placeholder="Email" type="text" class="form-control" name="login_name">
+					</div>
+					<div class="form-group">
+						<label for="login_pass">Password</label>
+						<input placeholder="Password" type="password" class="form-control" name="login_pass">
+					</div>
+						<button class="btn btn-primary btn btn-block">Connexion <span class="glyphicon glyphicon-log-in"></span></button>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
